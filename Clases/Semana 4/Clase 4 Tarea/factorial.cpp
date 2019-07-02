ac0f87a3f3e929ee64ca2d2b262eb9e6ec7d54a1
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-01 23:03:30 -0500

    Update factorial.cpp

--- Clases/Semana 4/Clase 4 Tarea/factorial.cpp
+++ Clases/Semana 4/Clase 4 Tarea/factorial.cpp
@@ -10,17 +10,17 @@ int n2 = 77;
 
 //Declaración funciones
 
-double fact(int);
+int fact(int);
 
 int main()
 {
-  cout<<fact(n1)<<endl;
+  //cout<<fact(n1)<<endl;
   cout<<fact(n2)<<endl;
   
   return 0;
 }
 
-double fact (int Val)
+int fact (int Val)
 {
   int cont = 1;
   for(int i = 1; i <= Val; i ++)
