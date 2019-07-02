a760be28375515f859493c1c272f2358e1109bfd
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-01 19:51:37 -0500

    Create producto.cpp

---
+++ Clases/Semana 4/Clase 4 Tarea/producto.cpp
@@ -0,0 +1,25 @@
+//Este código implemente el producto de variables
+
+#include <iostream>
+using namespace std;
+
+//Declaracion Variables
+
+int a = 2;
+float b = 3.0;
+
+//Declaracion funciones
+
+float product (int,float);
+
+int main()
+{
+  cout<< "el producto de a por b es:" << product(a,b)<< endl;
+  return 0;
+}
+
+float product (int val1, float val2)
+{
+  float val3 = val1*val2;
+  return val3;
+}
