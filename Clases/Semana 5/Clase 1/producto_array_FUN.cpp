743fcd6a45090a77e9b955c892ff704e5a696086
Default user <sm.morelli@uniandes.edu.co>
2019-07-02 15:54:55 +0000

    producto_array_FUN.cpp

--- Clases/Semana 5/Clase 1/producto_array_FUN.cpp
+++ Clases/Semana 5/Clase 1/producto_array_FUN.cpp
@@ -1,31 +1,31 @@
-//Este código halla el factorial de un número 
+//Este código halla el producto elemento a elemento y el producto punto de dos arrays
 
 #include <iostream>
 using namespace std;
 
 //Declaración Variables 
 
-int num;
+int array1[5] = {1,2,3,4,5};
+int array2[5] = {10,20,30,40,50};
 
 //Declaración funciones
 
-int fact(int);
+int product (int, int);
+int punto  (int, int);
 
 int main()
 {
-  cout<<"Número:" ;
-  cin >> num;
-  cout<<fact(num)<<endl;
-  
-  return 0;
+    cout<<"El producto elemento a elemento de los Arrays es" << product(array1,array2) << endl;
+    cout<<"El producto punto de los Arrays es" << punto(array1,array2) << endl;
+    return 0;
 }
 
-int fact (int Val)
+int producto (int Val1, int Val2)
 {
-  int cont = 1;
-  for(int i = 1; i <= Val; i ++)
-  {
-    cont = cont*i;
-  }
-  return cont;
+    
+}
+
+int punto (int Val1, int Val2)
+{
+    
 }
