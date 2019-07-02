3cf6dfe35e6449fcad311186f452828cd4322d7e
SebastianMorelli <52326060+SebastianMorelli@users.noreply.github.com>
2019-07-02 00:26:25 -0500

    Create primos.cpp

---
+++ Clases/Semana 4/Clase 4 Tarea/primos.cpp
@@ -0,0 +1,41 @@
+//Este código halla los números primos en un intervalo ingresado 
+
+#include <iostream>
+using namespace std;
+
+//Declaración Variables 
+
+int num1;
+int num2;
+
+//Declaración funciones
+
+int primos(int,int);
+
+int main()
+{
+  cout<<"Número incial del Intervalo:" ;
+  cin >> num1;
+  cout<<"Número final del Intervalo:" ;
+  cin >> num2
+  
+  cout<<primos(num1,num2)<<endl;
+  
+  return 0;
+}
+
+int primos (int Val1, int Val2){
+  for(int i = Val1; i <= Val2; i++){
+    int div = 0;
+    if(i%2 != 0){
+      for(int j=0; j <= i; j++){
+        if(i%j == 0){
+          div ++ ;
+        }
+      }
+    }
+    if(div == 2){
+      return i;
+    }
+  }
+}
